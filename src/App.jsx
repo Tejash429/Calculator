27cb1668a36b850ca54e1cbf3dc7c78aa17eda67
Usman Sabuwala (Max Programming) <51731966+max-programming@users.noreply.github.com>
2022-09-27 15:07:49 +0530

    Fix length typo

--- src/App.jsx
+++ src/App.jsx
@@ -72,7 +72,7 @@ function reducer(state, { type, payload }) {
       }
 
       if (state.currentOperand == null) return state;
-      if (state.currentOperand.lenght == 1) {
+      if (state.currentOperand.length == 1) {
         return {
           ...state,
           currentOperand: null,
